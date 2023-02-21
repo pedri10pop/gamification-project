e94f01ad957c9009028ce5913dba3450114c7268
Pedro Paulo <pedro.paulo.vianna@hotmail.com>
2023-02-21 10:23:19 -0300

    Updated TestUser
    Restarted the testing and class creation

--- tests/user/TestUser.java
+++ tests/user/TestUser.java
@@ -4,12 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-import achivements.AchievmentsHasNoPointsToAdd;
-import achivements.Achivement;
-import achivements.AchivementObjectHasNoPoints;
-import achivements.Badge;
-import achivements.Points;
-
 class TestUser {
 
 	@Test
@@ -19,77 +13,6 @@ class TestUser {
 		
 		assertEquals("Pedro", u.getName());
 		assertEquals(0, u.getPoints());
-	}
-	
-	@Test
-	void testCreateAnUserAndGiveAchivements() throws AchievmentsHasNoPointsToAdd, AchivementObjectHasNoPoints {
-		String userName = "Pedro";
-		User u = new User(userName);
-		
-		Achivement a = new Points("COMMENT", 5);
-		u.newAchivement(a);
-		
-		assertEquals("Pedro", u.getName());
-		assertEquals(5, u.getPoints());
-	}
-	
-	@Test
-	void testCreateAnUserAndGiveTowAchivements() throws AchievmentsHasNoPointsToAdd, AchivementObjectHasNoPoints {
-		String userName = "Pedro";
-		User u = new User(userName);
-		
-		Achivement a = new Points("COMMENT", 5);
-		Achivement b = new Points("LIKE IT", 1);
-		u.newAchivement(a);
-		u.newAchivement(b);
-		
-		assertEquals("Pedro", u.getName());
-		assertEquals(6, u.getPoints());
-	}
-	
-	@Test
-	void testCreateAnUserAndGiveBadge() {
-		String userName = "Pedro";
-		User u = new User(userName);
-		
-		Achivement a = new Badge("COMMENT", "YOU OWN IT");
-		
-		assertEquals("Pedro", u.getName());
-		assertEquals(0, u.getPoints());
-	}
-	
-	@Test
-	void testCreateAnUserAndThreePointAchivmentsEqual() throws UserHasNotThisAchivement, AchivementObjectHasNoPoints, AchievmentsHasNoPointsToAdd {
-		String userName = "Pedro";
-		User u = new User(userName);
-		
-		Achivement a = new Points("COMMENT", 5);
-		Achivement b = new Points("COMMENT", 5);
-		Achivement c = new Points("COMMENT", 5);
-		u.newAchivement(a);
-		u.newAchivement(b);
-		u.newAchivement(c);
-		
-		assertEquals("Pedro", u.getName());
-		assertEquals(15, u.getPoints());
-		assertEquals(15, u.getAchivementPoints("COMMENT"));
-	}
-	
-	@Test
-	void testCreateAnUserAndMultiplePoints() throws UserHasNotThisAchivement, AchivementObjectHasNoPoints, AchievmentsHasNoPointsToAdd {
-		String userName = "Pedro";
-		User u = new User(userName);
-		
-		Achivement a = new Points("COMMENT", 5);
-		Achivement b = new Points("COMMENT", 5);
-		Achivement c = new Points("COMMENT", 5);
-		u.newAchivement(a);
-		u.newAchivement(b);
-		u.newAchivement(c);
-		
-		assertEquals("Pedro", u.getName());
-		assertEquals(15, u.getPoints());
-		assertEquals(15, u.getAchivementPoints("COMMENT"));
 	}
 	
 }
